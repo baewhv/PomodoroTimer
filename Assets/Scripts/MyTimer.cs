0bb8f2c8c34b28caca151d616449c90ded24a77b
BaeJeongMin <baewhv@naver.com>
2020-02-19 19:43:09 +0900

    [ADD] : 업로드!

---
+++ Assets/Scripts/MyTimer.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class MyTimer : MonoBehaviour
+{
+    public float restTime = 300.0f;
+    public float workTime = 1500.0f;
+
+    public Slider slider;
+    public GameObject Pin;
+    public GameObject StartText;
+    public GameObject PauseText;
+    public GameObject ResetButton;
+
+    public Text CurrentTime;
+    public Text TotalTime;
+    public Text TimeMessage;
+
+    float CurrentTimeValue = 0.0f;
+    float CurrentTimeHour = 0;
+    float CurrentTimeMin = 0;
+    float CurrentTimeSec = 0;
+
+    float TotalTimeValue = 0.0f;
+    float TotalTimeHour = 0;
+    float TotalTimeMin = 0;
+    float TotalTimeSec = 0;
+
+    bool isStart;
+    bool isWork;
+
+    void RotatePin()
+    {
+        if (!isStart)
+        {
+            return;
+        }
+        Pin.transform.Rotate(0, 0, -1 * (360 / (restTime + workTime)) * Time.deltaTime);
+
+    }
+
+    public void StartPauseToggleTimer()
+    {
+        if (isStart)
+        {
+            //끄는 것
+            isStart = false;
+            ResetButton.SetActive(true);
+            PauseText.SetActive(false);
+            StartText.SetActive(true);
+        }
+        else
+        {
+            //켜는 것
+            isStart = true;
+            ResetButton.SetActive(false);
+            PauseText.SetActive(true);
+            StartText.SetActive(false);
+        }
+    }
+
+    public void ResetTimer()
+    {
+        InitTimer();
+    }
+
+
+    void InitTimer()
+    {
+        isStart = false;
+        isWork = true;
+        PauseText.SetActive(false);
+        ResetButton.SetActive(false);
+        slider.value = workTime / (restTime + workTime);
+        Pin.transform.rotation = Quaternion.Euler(0, 0, 0);
+        CurrentTimeValue = workTime;
+        TotalTimeValue = 0.0f;
+        CalcTime();
+    }
+
+    void SetTime()
+    {
+        if (!isStart)
+        {
+            return;
+        }
+        if (CurrentTimeValue < TotalTimeValue)
+        {
+            if (isWork)
+            {
+                isWork = false;
+                CurrentTimeValue += restTime;
+                TimeMessage.text = "휴식시간";
+            }
+            else
+            {
+                isWork = true;
+                CurrentTimeValue += workTime;
+                TimeMessage.text = "집중할 시간!";
+            }
+        }
+        CalcTime();
+
+        TotalTimeValue += Time.deltaTime;
+    }
+
+    void CalcTime()
+    {
+        CurrentTimeHour = (CurrentTimeValue - TotalTimeValue) / 3600;
+        CurrentTimeMin = ((CurrentTimeValue - TotalTimeValue) % 3600) / 60;
+        CurrentTimeSec = ((CurrentTimeValue - TotalTimeValue) % 3600) % 60;
+
+        TotalTimeHour = TotalTimeValue / 3600;
+        TotalTimeMin = (TotalTimeValue % 3600) / 60;
+        TotalTimeSec = (TotalTimeValue % 3600) % 60;
+
+        CurrentTime.text = string.Format("{0:D2} : {1:D2} : {2:D2}", (int)CurrentTimeHour, (int)CurrentTimeMin, (int)CurrentTimeSec);
+        TotalTime.text = string.Format("{0:D2} : {1:D2} : {2:D2}", (int)TotalTimeHour, (int)TotalTimeMin, (int)TotalTimeSec);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitTimer();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePin();
+        
+        SetTime();
+
+    }
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class Window : MonoBehaviour
+{
+
+
+    [DllImport("user32.dll")]
+    static extern int GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindowAsync(int hWnd, int nCmdShow);
+
+    [DllImport("user32.dll", EntryPoint = "MoveWindow")]
+    static extern int MoveWindow(int hwnd, int x, int y, int nWidth, int nHeight, int bRepaint);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongA")]
+    static extern int SetWindowLong(int hwnd, int nIndex, long dwNewLong);
+
+    //
+    [DllImport("user32.dll", EntryPoint = "GetWindowLongA")]
+    static extern long GetWindowLong(int hwnd, int nIndex);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    static extern bool SetWindowPos(int hwnd, int gwndInsertAfter, int x, int y, int cx, int cy, uint uFlag);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if !UNITY_EDITOR
+        int handle = GetForegroundWindow();
+
+        int fWidth = Screen.width;
+        int fHeight = Screen.height;
+
+        long lStyle = GetWindowLong(handle, -16);
+        lStyle &= ~(0x00C00000L | 0x00040000L | 0x20000000L | 0x01000000L | 0x00080000L);
+        SetWindowLong(handle, -16, lStyle);
+        long lExStyle = GetWindowLong(handle, -20);
+        lExStyle &= ~(0x00000001L | 0x00000200L | 0x00020000L);
+        SetWindowLong(handle, -20, lExStyle);
+        SetWindowPos(handle, 0, 0, 0, 0, 0, 0x0020 | 0x0002 | 0x0001 | 0x0004 | 0x0200);
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
